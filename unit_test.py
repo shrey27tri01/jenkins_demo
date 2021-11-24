aadbce092a7858df32ffa566373bc22ad9e08023
Shrey Tripathi <52366674+shrey27tri01@users.noreply.github.com>
2021-11-24 10:30:07 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -5,7 +5,7 @@ class testMulti(unittest.TestCase):
     def testMulti1(self):
         self.assertEqual(multiply(3, 4), 12)
     def testMulti2(self):
-        self.assertEqual(multiply(5, 6), 31)
+        self.assertEqual(multiply(5, 6), 30)
     def testMulti3(self):
         self.assertEqual(multiply(7, 7), 49)
 
